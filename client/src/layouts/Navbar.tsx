6fbdc93bbe9b2b961d0e4875891a0f71855b0cd5
Farouk Adeleke <fadeleke57@gmail.com>
2025-07-20 15:26:32 -0400

    agents tab

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -2,12 +2,7 @@
 
 import * as React from "react";
 import Link from "next/link";
-import {
-  CircleCheckIcon,
-  CircleHelpIcon,
-  CircleIcon,
-  List,
-} from "lucide-react";
+import { List } from "lucide-react";
 
 import {
   NavigationMenu,
@@ -20,44 +15,6 @@ import {
 } from "@/components/ui/navigation-menu";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
 
 export function Navbar() {
   return (
@@ -86,7 +43,7 @@ export function Navbar() {
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/cores">Cores</Link>
+            <Link href="/agents">Agents</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
 
