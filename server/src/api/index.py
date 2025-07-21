9c7cc3c2ab719e3807e75f0fb48d44c79112710d
Ty Todd <swagginty@gmail.com>
2025-07-20 23:37:05 -0400

    traces

--- server/src/api/index.py
+++ server/src/api/index.py
@@ -2,5 +2,6 @@ from src.api.user.index import router as user_router
 from src.api.repo.index import router as repo_router
 from src.api.auth.index import router as auth_router
 from src.api.contributor.index import router as contributor_router
+from src.api.trace.index import router as trace_router
 
-__all__ = ["user_router", "repo_router", "auth_router", "contributor_router"]
+__all__ = ["user_router", "repo_router", "auth_router", "contributor_router", "trace_router"]
